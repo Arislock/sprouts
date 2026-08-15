4bb4e0f14cd372ed56375e84ee676167aeafc5a1
Arislock <melodywang2244@gmail.com>
2026-08-15 13:03:28 -0400

    added design system

---
+++ src/app/theme/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  // brand
+  white: "#FFFFFF",
+  lightGreen: "#D3DCAD",
+  darkGreen: "#97AC4F",
+
+  bg: "#FDF9F0",
+
+  // tags
+  lightBlue: "#DEECF7",
+  green: "#C3D186",
+  purple: "#E9E5FF",
+  darkBlue: "#CDDFFF",
+  turqoise: "#DCF0E1",
+  pink: "#FFD9DC",
+  yellow: "#FEF1C9",
+  orange: "#FADAA4",
+} as const;
---
+++ src/app/theme/typography.ts
@@ -0,0 +1,38 @@
+export const typography = {
+  largeBold: {
+    fontFamily: "Fredoka-SemiBold",
+    fontSize: 24,
+    lineHeight: 29,
+  },
+  mediumBold: {
+    fontFamily: "Fredoka-SemiBold",
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  smallBold: {
+    fontFamily: "Fredoka-SemiBold",
+    fontSize: 14,
+    lineHeight: 17,
+  },
+
+  largeReg: {
+    fontFamily: "Fredoka-Medium",
+    fontSize: 24,
+    lineHeight: 29,
+  },
+  mediumReg: {
+    fontFamily: "Fredoka-Medium",
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  smallReg: {
+    fontFamily: "Fredoka-Medium",
+    fontSize: 14,
+    lineHeight: 17,
+  },
+  xsmallReg: {
+    fontFamily: "Fredoka-Medium",
+    fontSize: 10,
+    lineHeight: 12,
+  },
+} as const;
